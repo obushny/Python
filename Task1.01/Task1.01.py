98714f9e55a901f8ae5396d6424a48f7bb43d172
obushny <obushny@mail.ru>
2017-06-07 13:58:48 +0300

    Task1.01 done

--- Task1.01/Task1.01.py
+++ Task1.01/Task1.01.py
@@ -3,40 +3,39 @@ __author__ = 'sf2016'
 # encoding: utf-8
 import random
 from string import punctuation
-
-
-
+from string import digits
 
 def randomizer(text):
-    buffer=list(text[1:-1])
-    random.shuffle(buffer)
-    buffer.insert(0,text[0])
-    if len(text)!=1:
+    if len(text)<2:
+        buffer=text
+    else:
+        buffer=list(text[1:-1])
+        random.shuffle(buffer)
+        buffer.insert(0,text[0])
         buffer.append(text[-1])
-    text1=''.join(buffer)
-    return text1
+    return ''.join(buffer)
 
-def parser(text):
-    firstPart=[]
-    lastLatterIndex=0
+def analizer(text):
+    buffer=[]
+    temp=[]
     for i in text:
-        if i not in punctuation:
-            firstPart.append(i)
-            lastLatterIndex += 1
+        if (i not in punctuation) and (i not in digits):
+            buffer.append(i)
         else:
-            break
-    secondPart=text[lastLatterIndex:]
-    text1=[]
-    text1.append(''.join(randomizer(firstPart)))
-    text1.append(''.join(secondPart))
-    result=''.join(text1)
-    return result
+            bufferedText=randomizer(''.join(buffer))
+            temp.append(bufferedText)
+            temp.append(i)
+            buffer=[]
+    temp.append(randomizer(''.join(buffer)))
+    return ''.join(temp)
 
+text="По результатам усследований одного английского университета, не имеет значения, "
+text += 'в каком порядке расположены слова в слове. Главное, чтобы первая и последняя буквы были на месте. '
+text += 'Остальные буквы могут следовать в полном беспорядке, все равно текст читается без проблем. '
+text += 'Причиной этого является то, что мы не читаем каждую букву по отдельности, а все слово целиком'
 
-text="По результатам исследований одного английского института не имеет значения в каком порядке идут слова"
-text="вот результат: результат"
 bufferedText=text.split()
 for i in bufferedText:
-    print(parser(i),end=" ")
+    print(analizer(i),end=" ")
 
 
